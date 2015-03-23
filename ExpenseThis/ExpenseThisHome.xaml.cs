36581179de4eac6519e0656528a4678af1576ee7
Chris <chris.newell.88@gmail.com>
2015-03-23 15:33:34 -0500

    Resume code snippet
    code proof by example

---
+++ ExpenseThis/ExpenseThisHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseThis
+{
+    /// <summary>
+    /// Interaction logic for ExpenseThisHome.xaml
+    /// </summary>
+    public partial class ExpenseThisHome : Page
+    {
+        public ExpenseThisHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.FoundersListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        
+        }
+    }
+}
